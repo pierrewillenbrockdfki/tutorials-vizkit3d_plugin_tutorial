24d074d8d94ec4a4b7458097496a54e8f76124f7
Sascha Arnold <sascha.arnold@dfki.de>
2011-08-04 14:38:21 +0200

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vizkit_plugin_tutorial/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+	DummyProject::DummyClass dummyClass;
+	dummyClass.welcome();
+
+	return 0;
+}
